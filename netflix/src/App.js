1b8feb30970fffa8eb78dc014179155285d97d2c
Mansi Ganar <86571822+MansiGanar@users.noreply.github.com>
2021-10-01 13:20:57 +0200

    Navbar Completed

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,11 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Galleries from './components/Galleries'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Galleries from "./components/Galleries";
+import Navbarr from "./components/Navbarr";
 
 function App() {
   return (
     <div className="App">
+      <Navbarr />
       <Galleries />
     </div>
   );
